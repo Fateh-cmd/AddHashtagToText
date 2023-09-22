c934cc0c31656def805c3e933d49f9d76b29b4f1
fateh bouretoua <bouretoua_fateh@hotmail.com>
2023-09-22 23:50:07 +0200

    Add

---
+++ AddHashtagToText/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static string AddHashtagToText()
+    {
+
+        Console.Write($"Please enter some text: ");
+        string text = Console.ReadLine();
+
+        // Prefix the text entered with #
+        string result = $"#{text}";
+
+        return result;
+    }
+
+    static void Main()
+    {
+        string hashtagText = AddHashtagToText();
+        Console.WriteLine($"{hashtagText}");
+    }
+}
